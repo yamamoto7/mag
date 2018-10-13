74ce3f22bb3aa687baf11fce453c7e470e06f2a8
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-13 21:49:13 +0900

    コメント整理

--- app/javascript/components/users/SignIn.vue
+++ app/javascript/components/users/SignIn.vue
@@ -42,20 +42,6 @@ export default {
     async  newUserSession () {
       try {
         const response = await http.post('/api/users/sign_in', this.user)
-        // ローカルストレージに認証トークンを保存
-        // バリデーションのエラーが返ってきた場合の処理。
-        // Emailアドレスが既に使われているなど。
-        if (response.status === 202) {
-          // エラー内容をdataに入れる。
-          this.errors = response.data
-        } else {
-        // その中にある認証トークンをローカルストレージに保存。
-        // 今後はAPI通信を行う際にこの値をリクエストヘッダに含めることでサーバー上でログインしていると判断されるようになる。
-          await localStorage.setItem('Authorization', response.data.token)
-          // リダイレクト先指定。
-          await this.$router.go()
-          this.$router.push('/')
-        }
       } catch (error) {
         // サーバーもしくはネットワークのエラーが返ってきた場合の処理。
         console.log(error)
