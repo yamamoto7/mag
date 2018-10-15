74147dfc69fc08a3a840daa05b028e5347b2cb7e
fumihumi <t.fum1hum@gmail.com>
2018-10-15 23:32:56 +0900

    moduleの読み込みに失敗するバグ修正

--- src/packs/application.js
+++ src/packs/application.js
@@ -1,7 +1,7 @@
 /* eslint no-console:0 */
 import Vue from 'vue/dist/vue.esm'
-import router from './javascripts/router'
-import UserHeader from './javascripts/components/layout/UserHeader.vue'
+import router from '../javascripts/router'
+import UserHeader from '../javascripts/components/layout/UserHeader.vue'
 
 new Vue({
   router,
