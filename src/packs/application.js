9eacb25035e02bc932066245a8fdf568811a39de
fumihumi <t.fum1hum@gmail.com>
2018-10-17 11:28:26 +0900

    tmp

--- src/packs/application.js
+++ src/packs/application.js
@@ -1,8 +1,11 @@
 /* eslint no-console:0 */
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import Vue from 'vue/dist/vue.esm'
+import VueOnsen from 'vue-onsenui';
 import router from '../javascripts/router'
 import UserHeader from '../javascripts/components/layout/UserHeader.vue'
-
 new Vue({
   router,
   components: {
