38f724973b205abf8ad3efe057b2fdbeadff93ca
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-19 00:03:37 +0900

    vueファイル作成

--- src/javascripts/components/HomeTop.vue
+++ src/javascripts/components/HomeTop.vue
@@ -5,20 +5,23 @@
     <sign-up></sign-up>
     <sign-in></sign-in>
     <sign-out></sign-out>
+    <user-list></user-list>
   </div>
 </template>
 
 <script>
+import GetInfo from './users/GetInfo.vue'
 import SignUp from './users/SignUp.vue'
 import SignIn from './users/SignIn.vue'
 import SignOut from './users/SignOut.vue'
-import GetInfo from './users/GetInfo.vue'
+import UserList from './UserList.vue'
 export default {
   components: {
+    GetInfo,
     SignUp,
     SignIn,
     SignOut,
-    GetInfo
+    UserList
   }
 }
 </script>
---
+++ src/javascripts/components/UserList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>ユーザーリスト</h2>
+  </div>
+</template>
+
+<script>
+import http from '../http'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
