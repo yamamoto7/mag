9a6354a90f9c1e75c86d6ebaf86c685ca7ef0645
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-19 01:52:16 +0900

    :idのユーザー詳細返すAPI

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     get '/users/get_info' => 'users#get_info'
     get '/users' => 'users#index'
+    get '/users/:id' => 'users#show'
     namespace :users do
       resource :sign_in, only: [:create], controller: :sessions # users/sign_in
       resource :sign_out, only: [:destroy], controller: :sessions #users/sign_out
