27a5d22fe54dbb33b6984eae12688b4b5614f5ac
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-19 01:53:49 +0900

    ファイル作成

---
+++ src/javascripts/components/users/ShowDetail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>〇〇さんの詳細</h2>
+  </div>
+</template>
+
+<script>
+import http from '../http'
+
+export default {
+  data () {
+    return {
+    }
+  },
+  async mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
