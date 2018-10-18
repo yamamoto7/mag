e680d12e605c48743be39c4c6ef4136b8edfbb23
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-19 01:57:14 +0900

    コンポーネント内で表示。とりあえずid=1

--- src/javascripts/components/users/ShowDetail.vue
+++ src/javascripts/components/users/ShowDetail.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <h2>〇〇さんの詳細</h2>
+    <h2>〇〇さんの詳細(とりあえず1番)</h2>
+    {{ user.id }}/{{ user.email }}
   </div>
 </template>
 
 <script>
-import http from '../http'
+import http from '../../http'
 
 export default {
   data () {
     return {
+      user: ''
     }
   },
   async mounted () {
+    const response = await http.get('/api/users/1')
+    this.user = response.data
   },
   methods: {
   }
