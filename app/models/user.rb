4dba1e332b56ae9879904556670e7e0d73b6bfa1
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-21 17:57:13 +0900

    userとlikes_user間のアソシエージョン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :likes_items, class_name: "LikesUser", foreign_key: :user_id
+  has_many :likes_users, through: :likes_items
+  has_many :liked_items, class_name: "LikesUser", foreign_key: :to_likes_user_id
+  has_many :liked_users, through: :liked_items
 end
