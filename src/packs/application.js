b67a041840b13eba6522b3fcdcc8d3a2b7bed39f
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-22 04:55:02 +0900

    ログイン、登録ページ専用コンポーネント表示

--- src/packs/application.js
+++ src/packs/application.js
@@ -2,10 +2,14 @@
 import Vue from 'vue/dist/vue.esm'
 import router from '../javascripts/router'
 import UserHeader from '../javascripts/components/layout/UserHeader.vue'
+import UserSignIn from '../javascripts/components/users/SignIn.vue'
+import UserSignUp from '../javascripts/components/users/SignUp.vue'
 
 new Vue({
   router,
   components: {
-    UserHeader
+    UserHeader,
+    UserSignIn,
+    UserSignUp
   },
 }).$mount('#app')
