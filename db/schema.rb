f4fcbe228c3cad09764e1f8f21678f7caa86d956
fumihumi <t.fum1hum@gmail.com>
2018-10-22 10:18:08 +0900

    surveysテーブルにアンケートが有効かどうかのフラグ 'active'を追加
    - 複数のアンケートの同時実施を可能にするため。
    - アンケートを追加実施する際に既存の物を論理削除するようにするため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_083100) do
+ActiveRecord::Schema.define(version: 2018_10_22_011015) do
 
   create_table "chat_messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2018_10_21_083100) do
 
   create_table "surveys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
+    t.boolean "active", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
