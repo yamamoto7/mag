f5e4bee03ddd36108d9bdaa26868d8c532ec4106
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-23 11:03:14 +0900

    フッターのコンポーネントを作成して表示

---
+++ src/javascripts/components/layout/UserFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    ログイン済みフッター
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+@import '../../../style/layout/user_footer.scss'
+</style>
