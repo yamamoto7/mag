399badb0c602d0610550013ee726b1c369c0ec36
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-23 20:44:16 +0900

    リセットcss読み込み

--- src/packs/application.js
+++ src/packs/application.js
@@ -7,9 +7,6 @@ import UserFooter from '../javascripts/components/layout/UserFooter.vue'
 import UserSignIn from '../javascripts/components/users/SignIn.vue'
 import UserSignUp from '../javascripts/components/users/SignUp.vue'
 
-// リセットcssとか読み込む用
-import './style.scss'
-
 Vue.prototype.$cable  = ActionCable.createConsumer('ws:localhost:5000/cable')
 
 new Vue({
