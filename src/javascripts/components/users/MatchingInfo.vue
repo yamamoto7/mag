84174e0258e8b021f8b1b7c349404b00222d93b2
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-23 21:27:04 +0900

    マッチング一覧とルーム一覧の親コンポーネント作成

---
+++ src/javascripts/components/users/MatchingInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import http from '../../http'
+
+export default {
+  data () {
+    return {
+      users: []
+    }
+  },
+  async mounted () {
+ },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+@import '../../../style/users/matching_info.scss'
+</style>
--- src/javascripts/router/index.js
+++ src/javascripts/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HomeTop from '../components/HomeTop.vue'
 import UserList from '../components/UserList.vue'
 import ReceivedLikesList from '../components/users/ReceivedLikesList.vue'
+import MatchingInfo from '../components/users/MatchingInfo.vue'
 import UserShowDetail from '../components/users/ShowDetail.vue'
 import UserChatRoomList from '../components/users/ChatRoomList.vue'
 import UserChatRoom from '../components/users/ChatRoom.vue'
@@ -27,6 +28,11 @@ export default new Router({
       path: '/likes',
       name: 'ReceivedLikesList',
       component: ReceivedLikesList
+    },
+    {
+      path: '/likes',
+      name: 'MatchingInfo',
+      component: MatchingInfo
     },
     {
       path: '/users/chats/:room_id',
