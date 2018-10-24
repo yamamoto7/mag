8b61b8ba0c363c7126988fdbbfd13d25b1f5ea38
fumihumi <t.fum1hum@gmail.com>
2018-10-24 22:45:39 +0900

    共通して表示する箇所の設定など

---
+++ src/javascripts/screens/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class='screen main'>
+    <main-header show-back-btn="true" show-notification-btn="true"/>
+    <main-footer />
+  </div>
+</template>
+
+<script>
+import MainHeader from '../components/layout/Header.vue'
+import MainFooter from '../components/layout/Footer.vue'
+
+export default {
+  components: {
+    MainHeader,
+    MainFooter,
+  }
+}
+</script>
+
+<style scoped>
+</style>
