bf44c2250fc65e15bd17c2f55b887021f4c1e2c5
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-25 23:51:04 +0900

    チャット画面でアイコン表示される

--- app/controllers/api/users/images_controller.rb
+++ app/controllers/api/users/images_controller.rb
@@ -8,9 +8,13 @@ class Api::Users::ImagesController < ApplicationController
   end
   def get_top_image
     user = User.find(params[:user_id])
-    image = user.user_images.first
-    ActiveStorage::Current.host = request.base_url
-    render json: image, include: [{profile_image: {include: {blob: {methods: :service_url}}}}]
+    if user.user_images.exists? then
+      image = user.user_images.first
+      ActiveStorage::Current.host = request.base_url
+      render json: image, include: [{profile_image: {include: {blob: {methods: :service_url}}}}]
+    else
+      render json: false
+    end
   end
   def create
     image = current_user.user_images.create!(user_image_params)
