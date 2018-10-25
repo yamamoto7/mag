8ab8bd6212d350922c359542eec006bff9dc1b7d
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-26 04:17:05 +0900

    登録画面

--- src/javascripts/components/users/SignUp.vue
+++ src/javascripts/components/users/SignUp.vue
@@ -1,37 +1,51 @@
 <template>
   <div>
-    <h2>ユーザー登録</h2>
-    <div>フルネーム</div>
-      {{ this.errors }}
-    <input type="text" v-model="user.first_name" placeholder="姓"/>
-    <input type="text" v-model="user.last_name" placeholder="名"/><br>
-    <input type="text" v-model="user.first_kana" placeholder="セイ"/>
-    <input type="text" v-model="user.last_kana" placeholder="メイ"/><br>
+    <h2>基本情報入力</h2>
+    <div class="wrap">
+      <div class="label-box"><span class="label">フルネーム</span><span class="hissu">※必須</span>{{ this.errors['first_kana'] }}{{ this.errors['last_kana'] }}{{ this.errors['first_name'] }}{{ this.errors['last_name'] }}</div>
+      <input class="text-form" type="text" v-model="user.first_name" placeholder="姓"/>
+      <input class="text-form" type="text" v-model="user.last_name" placeholder="名"/><br>
+      <input class="text-form" type="text" v-model="user.first_kana" placeholder="セイ"/>
+      <input class="text-form" type="text" v-model="user.last_kana" placeholder="メイ"/><br>
 
-    <select v-model="user.birth_year" @change="get_days">
-      <option v-for="n in 100" :value="n + 1917">
-        {{ n + 1917 }}
-      </option>
-    </select>年
-    <select v-model="user.birth_month" @change="get_days">
-      <option v-for="n in 12" :value="n">
-        {{ n }}
-      </option>
-    </select>月
-    <select v-model="user.birth_date">
-      <option v-for="n in parseInt(days_max, 10)" :value="n">
-        {{ n }}
-      </option>
-    </select>日
+      <div class="label-box"><span class="label">性別</span><span class="hissu">※必須</span>{{ this.errors['sex'] }}</div>
+      <div class="radio-form">
+        <input class="radio02-input" type="radio" id="one" value="One" v-model="user.sex">
+        <label for="one">男性</label>
+      </div>
+      <div class="radio-form">
+        <input class="radio02-input" type="radio" id="two" value="Two" v-model="user.sex">
+        <label for="two">女性</label>
+      </div>
 
-    <div>メールアドレス</div>
-    {{ this.errors['email'] }}
-    <input v-model="user.email" /><br>
-    <div>パスワード</div>
-    <input type="password" v-model="user.password" /><br>
-    <div>電話番号</div>
-    <input v-model="user.phone_number" placeholder="入力してください"/><br>
-    <button type="submit" @click="submitUser">ユーザー登録</button>
+
+      <div class="label-box"><span class="label">生年月日</span><span class="hissu">※必須</span>{{ this.errors['birth_year'] }}{{ this.errors['birth_month'] }}{{ this.errors['birth_date'] }}</div>
+      <select v-model="user.birth_year" @change="get_days">
+        <option v-for="n in 90" :value="n + 1910">
+          {{ n + 1910 }}
+        </option>
+      </select>
+      <select v-model="user.birth_month" @change="get_days">
+        <option v-for="n in 12" :value="n">
+          {{ n }}
+        </option>
+      </select>
+      <select v-model="user.birth_date">
+        <option v-for="n in parseInt(days_max, 10)" :value="n">
+          {{ n }}
+        </option>
+      </select>
+
+      <div class="label-box"><span class="label">メールアドレス</span><span class="hissu">※必須</span>{{ this.errors['email'] }}</div>
+      
+      <input class="text-form text-form-long" v-model="user.email" /><br>
+      <div class="label-box"><span class="label">パスワード</span><span class="hissu">※必須</span>{{ this.errors['password'] }}</div>
+      <input class="text-form text-form-long" type="password" v-model="user.password" /><br>
+      <input class="text-form text-form-long" type="password" placeholder="もう一度入力してください" /><br>
+      <div class="label-box"><span class="label">電話番号</span><span class="hissu">※必須</span>{{ this.errors['phone_number'] }}</div>
+      <input class="text-form text-form-long" v-model="user.phone_number"/><br>
+      <button class="btn" type="submit" @click="submitUser">次へ</button>
+    </div>
   </div>
 </template>
 
