469a8d0ea34f5db1f5bc80781cec03cc4ce34989
fumihumi <t.fum1hum@gmail.com>
2018-10-26 01:44:59 +0900

    記憶が定かではないが無駄な処理を消したはず

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except: %i[auth]
 
-  def index; end
+  # def index; end
   def top; end
   def auth; end
   def survey; end
 
   private
   def redirect_root
-    redirect_to :index and return if current_user
+    redirect_to :top and return if current_user
   end
 end
