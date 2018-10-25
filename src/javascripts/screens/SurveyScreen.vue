234fac8a5d296ba9076a3013b7900c6121ce2f50
fumihumi <t.fum1hum@gmail.com>
2018-10-25 18:43:42 +0900

    landingPage

--- src/javascripts/screens/SurveyScreen.vue
+++ src/javascripts/screens/SurveyScreen.vue
@@ -1,21 +1,22 @@
 <template>
-  <div>
-    <h2 class='survey screen'>
-      アンケート画面
-    </h2>
-
-    <div>
+  <div class='survey screen'>
+    <div v-if='this.$route.params.question_id'>
       <survey-question-list
         v-bind:survey_title="survey.title"
         v-bind:survey_id="survey.id"
       />
     </div>
+    <div v-else>
+      <survey-landing-page />
+    </div>
+
   </div>
 </template>
 
 <script>
   import http from '../http'
   import SurveyQuestionList from '../components/surveys/SurveyQuestionList.vue'
+  import SurveyLandingPage from '../components/surveys/SurveyLandingPage.vue'
 
   export default {
     data () {
@@ -33,6 +34,7 @@
     },
     components: {
       SurveyQuestionList,
+      SurveyLandingPage,
     }
 }
 </script>
