1ac6f552068b8be66a571e4c96e8f402a96d397c
yamamoto <yamamoto.kenta.infratop@gmail.com>
2018-10-27 00:33:05 +0900

    get_top_image削除

--- app/controllers/api/users/images_controller.rb
+++ app/controllers/api/users/images_controller.rb
@@ -6,16 +6,6 @@ class Api::Users::ImagesController < ApplicationController
     ActiveStorage::Current.host = request.base_url
     render json: images, include: [{profile_image: {include: {blob: {methods: :service_url}}}}]
   end
-  def get_top_image
-    user = User.find(params[:user_id])
-    if user.user_images.exists? then
-      image = user.user_images.first
-      ActiveStorage::Current.host = request.base_url
-      render json: image, include: [{profile_image: {include: {blob: {methods: :service_url}}}}]
-    else
-      render json: false
-    end
-  end
   def create
     image = current_user.user_images.create!(user_image_params)
   end
