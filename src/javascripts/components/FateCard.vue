af7f991bc72cde7a16883cf3a0f6981df304f4b4
fumihumi <t.fum1hum@gmail.com>
2018-10-26 21:17:54 +0900

    component

---
+++ src/javascripts/components/FateCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class='home--users'>
+    <div class='d-flex flex-row card-container'>
+      <img class='user-img' src="/assets/1_prof.jpg">
+      <div class='user-prf'>
+        <p class='mt-1 w-100'>あや 愛知</p>
+        <p class='mt-1 w-100'>
+          職業
+          <span>事務員</span>
+        </p>
+        <p class='mt-1 w-100'>
+          年収
+          <span>~300</span>
+        </p>
+        <p class='profile'>いい人と 出会いたいと 考えてます。 よろしく お願いします。</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+  // import http from '../../http'
+
+  export default {
+    data () {
+      return {
+      }
+    },
+    async mounted () {
+    },
+    methods: {
+    },
+    components: {
+    }
+  }
+</script>
+
+<style scoped>
+  @import '../../style/fate-card.scss'
+</style>
+
--- src/javascripts/screens/main.vue
+++ src/javascripts/screens/main.vue
@@ -3,26 +3,8 @@
     <!-- <user-list/> -->
     <div class='home-container'>
       <p class='home--title'>今日の運命の相手かも</p>
-      <div class='home-wrapper'>
-        <!--  -->
-        <div class='home--users'>
-          <div class='d-flex flex-row card-container'>
-            <img class='user-img' src="/assets/1_prof.jpg">
-            <div class='user-prf'>
-              <p class='mt-1 w-100'>あや 愛知</p>
-              <p class='mt-1 w-100'>
-                職業
-                <span>事務員</span>
-              </p>
-              <p class='mt-1 w-100'>
-                年収
-                <span>~300</span>
-              </p>
-              <p class='profile'>いい人と 出会いたいと 考えてます。 よろしく お願いします。</p>
-            </div>
-          </div>
-        </div>
-        <!-- k -->
+      <div class='fate-wrapper'>
+        <fate-card />
       </div>
     </div>
 
@@ -31,10 +13,12 @@
 
 <script>
 import UserList from '../components/UserList.vue'
+import FateCard from '../components/FateCard.vue'
 
 export default {
   components: {
     UserList,
+    FateCard,
   }
 }
 </script>
