9007bb64a94bb8681ffc38980d5234dcb4c085bd
fumihumi <t.fum1hum@gmail.com>
2018-10-26 22:47:54 +0900

    運命ページ

--- src/javascripts/components/FateCard.vue
+++ src/javascripts/components/FateCard.vue
@@ -3,16 +3,14 @@
     <div class='d-flex flex-row card-container'>
       <img class='user-img' src="/assets/1_prof.jpg">
       <div class='user-prf'>
-        <p class='mt-1 w-100'>あや 愛知</p>
-        <p class='mt-1 w-100'>
-          職業
-          <span>事務員</span>
+        <p class='mt-1 w-100 name'>あや 愛知</p>
+        <p class='mt-1 w-100 job sub-label'>職業
+          <span class='value'>事務員</span>
         </p>
-        <p class='mt-1 w-100'>
-          年収
-          <span>~300</span>
+        <p class='mt-1 w-100 salaly sub-label'>年収
+          <span class='value'>~300</span>
         </p>
-        <p class='profile'>いい人と 出会いたいと 考えてます。 よろしく お願いします。</p>
+        <p class='profile'>いい人と出会いたいと考えてます。よろしくお願いします。</p>
       </div>
     </div>
   </div>
@@ -37,6 +35,6 @@
 </script>
 
 <style scoped>
-  @import '../../style/fate-card.scss'
+  @import '../..//style/fate-card.scss'
 </style>
 
