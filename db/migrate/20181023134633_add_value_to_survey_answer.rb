a46856b95d828af2c16d76581276956b0bd567a5
yamamoto kenta <yamamoto7.pgming@gmail.com>
2019-05-29 21:56:15 +0900

    [db: remigration]

--- db/migrate/20181023134633_add_value_to_survey_answer.rb
+++ db/migrate/20181023134633_add_value_to_survey_answer.rb
@@ -1,5 +1,5 @@
 class AddValueToSurveyAnswer < ActiveRecord::Migration[5.2]
   def change
-    add_column :survey_answers, :value, :integer, null: false, after: :survey_questions_id
+    add_column :survey_answers, :value, :integer, after: :survey_questions_id
   end
 end
